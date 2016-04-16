93785bed07cd07406a7d460bb2d113bf5ffe328c
CSparano-Huiban <csparano@mit.edu>
2016-04-15 20:31:52 -0400

    made viewers

--- mobile/src/main/java/edu/mit/outnabout/MainActivity.java
+++ mobile/src/main/java/edu/mit/outnabout/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.mit.outnabout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -48,5 +49,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startWebView(View view) {
+        Intent i = new Intent(this, WebViewers.class);
+        startActivity(i);
     }
 }
