af4996b8652b7df86da32d428cef54130cd85e5f
mirim <kish9nine@gmail.com>
2016-05-04 14:36:51 -0400

    Splash screen shows for at least 5 seconds.

--- mobile/src/main/java/edu/mit/outnabout/SplashActivity.java
+++ mobile/src/main/java/edu/mit/outnabout/SplashActivity.java
@@ -4,14 +4,35 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class SplashActivity extends AppCompatActivity {
+
+    private static final ScheduledExecutorService worker =
+            Executors.newSingleThreadScheduledExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
-        finish();
+        Thread timer=new Thread()
+        {
+            public void run() {
+                try {
+                    sleep(5000); // show splash screen for at least 5 seconds. 
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    Intent i=new Intent(SplashActivity.this, HomeActivity.class);
+                    finish();
+                    startActivity(i);
+                }
+            }
+        };
+        timer.start();
     }
 }
