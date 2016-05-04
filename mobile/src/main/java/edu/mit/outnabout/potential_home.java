9697d90fa7e6690d2a5bde3b31d73bda1059f92a
mirim <kish9nine@gmail.com>
2016-05-03 22:41:59 -0400

    Added a launcher icon.

--- mobile/src/main/java/edu/mit/outnabout/potential_home.java
+++ mobile/src/main/java/edu/mit/outnabout/potential_home.java
@@ -104,16 +104,6 @@ public class potential_home extends FragmentActivity implements GoogleApiClient.
         find();
 
         Log.v("I hate this shit","I am doing something");
-
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-
     }
 
     @Override
